98ab5037a3f655ce6ca1f48d21fb1ccec0dee050
Simone Sabbatini <sabbatini.simone@gmail.com>
2018-07-12 22:14:00 +0100

    started card structure

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import classes from './Player.css';
-
+// https://jsfiddle.net/Zeaklous/mmrnvwhn/8/
 const player = (props) => (
     <div className={classes.player}>
-        {props.player.firstname}
+        <div className={classes.player__picture}>
+            <img src={props.player.picture} alt={props.player.firstname} />
+        </div>
+        <div className={classes.player__name}>
+            <p><span>{props.player.firstname}</span> <span>{props.player.lastname}</span></p>
+            <p>({props.player.shortname})</p>
+        </div>
     </div>
 );
 
