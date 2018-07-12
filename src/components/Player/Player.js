8a8752dc729031e0bb7698e187241eea17eed9a4
Simone Sabbatini <simone.sabbatini@bynd.com>
2018-07-12 13:51:00 +0100

    Added api handler; added list and player component

---
+++ src/components/Player/Player.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const player = (props) => (
+    <div>{props.player.firstname}</div>
+);
+
+export default player;
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,10 +1,40 @@
 import React, { Component, Fragment } from 'react';
+import api from '../../helpers/api';
+
+// Components
+import List from '../../components/List/List';
+import Player from '../../components/Player/Player';
 
 class Home extends Component {
+
+    state = {
+        error: false,
+        players: []
+    };
+
+    componentDidMount() {
+        api.getPlayers()
+            .then(response => {
+                console.log(response.data);
+                this.setState({ players: response.data.players });
+            })
+            .catch(error => this.setState({ error: true }));
+    }
+
     render() {
+        let players = <h1>Some error occurs fetching data. Please retry</h1>;
+
+        if (!this.state.error) {
+            players = this.state.players.map(player => {
+                return <Player player={player} key={player.shortname} />;
+            });
+        }
+
         return (
             <Fragment>
-                <div> Home container </div>
+                <List>
+                    {players}
+                </List>
             </Fragment>
         )
     }
