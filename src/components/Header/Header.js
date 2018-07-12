2e0adaa0bafb86799f5b9b9fc150974428d41d46
Simone Sabbatini <simone.sabbatini@bynd.com>
2018-07-12 17:31:59 +0100

    Added logo and new favicon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import classes from './Header.css';
+import logo from '../../Eurosport_logo.svg';
 
 const header = () => (
     <header className={classes.header}>
+        <img src={logo} alt="logo" />
         Eurosport Player Stats
     </header>
 );
